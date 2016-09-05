5bd50d2b640956a850d756f66b33505cf66db71f
Jacob <jacob.reed2017@gmail.com>
2016-09-05 16:19:17 -0500

    Initial commit

---
+++ app/src/main/java/com/reeddev/jacobreed/a618storeapp/juiceview.java
@@ -0,0 +1,13 @@
+package com.reeddev.jacobreed.a618storeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class juiceview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juiceview);
+    }
+}
