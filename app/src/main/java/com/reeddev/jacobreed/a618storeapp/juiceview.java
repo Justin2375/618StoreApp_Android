b5ce58519aeb36f614698205067d3817d7400fab
Jacob <jacob.reed2017@gmail.com>
2016-09-05 16:47:18 -0500

    added documentation

--- app/src/main/java/com/reeddev/jacobreed/a618storeapp/juiceview.java
+++ app/src/main/java/com/reeddev/jacobreed/a618storeapp/juiceview.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class juiceview extends AppCompatActivity {
 
+    /* Creates the new activity */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
